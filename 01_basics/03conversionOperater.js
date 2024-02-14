0cb57250687b3b91308ef05c4d1409fd865759a3
Vikram kumar <111545034+thevikramkunal@users.noreply.github.com>
2024-02-14 13:20:31 +0000

    data type conversion

---
+++ 01_basics/03conversionOperater.js
@@ -0,0 +1,21 @@
+let score = "hitesh"
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+
+
+console.log(valueInNumber);
+  let isloggedIn=1
+  let booleanIsLoggedIn= Boolean(isloggedIn)
+  console.log(booleanIsLoggedIn);
+
+//1=> true;0=> false
+//""=>false 
+//"hitesh"=> true 
+let someNumber=33
+let stringNumber= string(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
