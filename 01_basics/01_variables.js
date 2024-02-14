2ca7c02ba78b5561ddb7b62a37cef4e49248df34
Vikram kumar <111545034+thevikramkunal@users.noreply.github.com>
2024-02-14 11:20:52 +0000

    variablesand data type

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,7 @@
+const accountid =14453
+let accountemail ="vikram@google.com"
+var accountpassword ="vikramyadav@123456"
+acooountCity = "Araria"
+
+accountemail= "vsdfh@hotemail.com"
+console.table([accountid,accountemail,accountpassword]);
---
+++ 01_basics/02datatypes.js
@@ -0,0 +1,18 @@
+console.log(9+9);
+console.log("vikram");
+let name = "vikram"
+let age= 21
+let isloggedin =false 
+// number => 2 to power 53 
+// bigint
+//string=> ""
+// boolean => true/false 
+// null => standalon value 
+//undefined =>
+//symbol => uniqu
+
+
+
+// object 
+console.log(typeof undefined);
+console.log(typeof null);
