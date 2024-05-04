dafca4ca9c96d49b8ae8838d53bd2993c2f196b5
Vikram kumar <111545034+thevikramkunal@users.noreply.github.com>
2024-05-04 07:35:10 +0000

    heap

---
+++ 01_basics/stack_heap.js
@@ -0,0 +1,16 @@
+// let myyoutubename ="vikramyadav"
+// let anothername=myyoutubename;
+// anothername="charmingdev"
+// console.log(myyoutubename);
+// console.log(myyoutubename);
+
+
+let user1 ={
+    email: "vikram@gmail.com",
+    upi:"677861hasdfh@ybl"
+ 
+};
+let user2 = user1;
+user2.email= "chaming@gmail.com"
+console.log(user1.email);
+console.log(user2.email);
