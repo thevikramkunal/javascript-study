fa7add6807f52e57d7ee7f3e7bf240103836a16b
Vikram kumar <111545034+thevikramkunal@users.noreply.github.com>
2024-05-14 11:34:05 +0000

    date and array

---
+++ 01_basics/date.js
@@ -0,0 +1,16 @@
+/* let myDate= new Date()
+console.log(myDate.toString ());
+console.log(myDate.toDateString ());
+console.log(myDate.toISOString ());
+console.log(myDate.toJSON ());
+console.log(myDate.toLocaleDateString ());
+console.log(myDate.toLocaleString ());*/
+
+/*let mycreatedDate = new Date(2024,5,23)
+console.log(mycreatedDate.toDateString());*/
+let mycreatedDate = new Date("01-14-2024")
+let myTimestamp =Date.now()
+console.log(mycreatedDate.getTime())
+console.log(myTimestamp);
+// console.log(Date.now()/1000);
+console.log(math.floor(Date.now()/1000));
