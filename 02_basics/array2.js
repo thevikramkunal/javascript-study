e5bab9e9629e43c7adcacf11460d847f1886d3c0
Vikram kumar <111545034+thevikramkunal@users.noreply.github.com>
2024-05-15 07:21:12 +0000

    complete array

--- 02_basics/array2.js
+++ 02_basics/array2.js
@@ -1,2 +1,9 @@
 //puchhenge  array se 
 console.log(Array.isArray("vikram"));
+console.log(Array.from("vikram"));
+console.log(Array.from({name: "vikram"}));// intresting hai aage pdhenge 
+let score1=100
+let score2=200
+let score3=400
+// multiple vareable ko array mai covert 
+console.log(Array.of(score1,score2,score3));
