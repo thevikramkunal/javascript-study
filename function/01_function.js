4b27ff2d52bd56cbc17885da319495ff236466c2
Vikram kumar <111545034+thevikramkunal@users.noreply.github.com>
2024-05-22 21:33:34 +0000

    function

---
+++ function/01_function.js
@@ -0,0 +1,23 @@
+/*function myname() {
+    name= "vikram",
+    id= 43,
+    address = 8
+}
+myname()*/
+
+
+/*
+function addtwoNumber(number1,number2) // peramiter
+{
+    console.log(number1+number2);
+}
+addtwoNumber(56,76) //argument*/
+
+function addtwoNumber(number1,number2) 
+{
+    let result = number1 + number1
+    console.log("total");
+   return result
+}
+ const result =addtwoNumber(5,7)
+ console.log("Result", result);
