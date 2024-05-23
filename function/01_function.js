c06afd0cf7afffb084291ecdc6cd6d06fce42b33
Vikram kumar <111545034+thevikramkunal@users.noreply.github.com>
2024-05-23 10:14:03 +0000

    scop function

--- function/01_function.js
+++ function/01_function.js
@@ -13,7 +13,9 @@ function addtwoNumber(number1,number2) // peramiter
 }
 addtwoNumber(56,76) //argument*/
 
-function addtwoNumber(number1,number2) 
+
+
+ /*function addtwoNumber(number1,number2) 
 {
     let result = number1 + number1
     console.log("total");
@@ -21,3 +23,52 @@ function addtwoNumber(number1,number2)
 }
  const result =addtwoNumber(5,7)
  console.log("Result", result);
+
+
+ */
+
+
+
+
+
+ function loginUserMessage(username = "sam"){
+    if(!username){
+        console.log("PLease enter a username");
+        return
+    }
+    return `${username} just logged in`
+}
+
+// console.log(loginUserMessage("hitesh"))
+// console.log(loginUserMessage("hitesh"))
+
+
+function calculateCartPrice(val1, val2, ...num1){
+    return num1
+}
+
+// console.log(calculateCartPrice(200, 400, 500, 2000))
+
+const user = {
+    username: "hitesh",
+    prices: 199
+}
+
+function handleObject(anyobject){
+    console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`);
+}
+
+// handleObject(user)
+handleObject({
+    username: "sam",
+    price: 399
+})
+
+const myNewArray = [200, 400, 100, 600]
+
+function returnSecondValue(getArray){
+    return getArray[1]
+}
+
+// console.log(returnSecondValue(myNewArray));
+console.log(returnSecondValue([200, 400, 500, 1000]));
