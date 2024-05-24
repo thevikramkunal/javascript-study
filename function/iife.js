def4d742410f2c1399e2e726d461a9cb22be352f
Vikram kumar <111545034+thevikramkunal@users.noreply.github.com>
2024-05-24 07:04:23 +0000

    iife

---
+++ function/iife.js
@@ -0,0 +1,13 @@
+// ()  ()
+(function hey(){
+    console.log(`db connected`);
+
+})
+();
+
+
+// hey()
+((name)=> {
+    console.log(`db connected two ${name} `);// variable pass kar sakte hai ${}
+})
+('vikram')
